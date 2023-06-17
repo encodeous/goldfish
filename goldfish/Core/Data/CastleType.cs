0a1e106c9fb98e50271937ec3d822b129f3ea89a
encodeous <encodeous7@gmail.com>
2023-06-16 22:58:49 -0400

    Track files.

---
+++ goldfish/Core/Data/CastleType.cs
@@ -0,0 +1,10 @@
+﻿namespace goldfish.Core.Data;
+
+[Flags]
+public enum CastleType
+{
+    WhiteQs = 1, // castling queenside
+    BlackQs = 4,
+    WhiteKs = 2, // castling king side
+    BlackKs = 8
+}
---
+++ goldfish/Core/Data/Side.cs
@@ -0,0 +1,8 @@
+﻿namespace goldfish.Core.Data;
+
+public enum Side
+{
+    Black = 0,
+    White = 1,
+    None = 2 // for spaces only
+}
